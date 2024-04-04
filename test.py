3ecfd6dcb897376bf573b5ab047fb4f6ed9b23e0
jharjharbink <gustavetoison254@gmail.com>
2024-04-04 16:50:07 +0200

    first commit

--- test.py
+++
@@ -1,3 +0,0 @@
-  File "C:\Users\gusta\PycharmProjects\scrap_nodata\scrap_nodata\pipelines.py", line 144, in process_item
-    comment_number = int(comment_number.split(' '))  # Extraire le nombre de commentaires
-TypeError: int() argument must be a string, a bytes-like object or a real number, not 'list'
